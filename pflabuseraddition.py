0b584a69b1de5c6e018c3e33a64ca1921dc56441
dinithivihanga <dinithivihangaf5@gmail.com>
2026-04-21 15:18:39 +0530

    Create pflabuseraddition.py

---
+++ pflabuseraddition.py
@@ -0,0 +1,8 @@
+while True:
+    count = 1
+    total = 0
+    while count <= 5:
+        num = int(input(f"enter the num (count)")
+        total += num
+        count += 1
+    print("Total is:", Total)
